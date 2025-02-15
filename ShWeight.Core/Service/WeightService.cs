081c7ee314ed99d746566ad196b0ec6ff966ea87
huzy2025 <1726130@qq.com>
2025-02-15 13:10:31 +0800

    更新

---
+++ ShWeight.Core/Service/WeightService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShWeight.Core.Service
+{
+    public class WeightService
+    {
+    }
+}
